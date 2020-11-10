cbe49e66b1a701049096f19ae6ccb4a02d320d01
Sangki Kim <goodki283@gmail.com>
2020-11-11 00:08:23 +0900

    Initial Commit

---
+++ sangkiTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sangkiTable
+//
+//  Created by キム・サンギ on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
